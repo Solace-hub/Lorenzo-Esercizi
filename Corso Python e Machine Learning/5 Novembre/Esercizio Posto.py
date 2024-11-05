e32bb259e46493a01e410664bd3cb5fab23c9429
Solace-hub <fbxeim@gmail.com>
2024-11-05 17:43:50 +0100

    Esercizio Posto
    Struttura dell'esercizio

---
+++ Corso Python e Machine Learning/5 Novembre/Esercizio Posto.py
@@ -0,0 +1,63 @@
+class Posto:
+    pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def prenota(self):
+    pass
+
+def libera(self):
+    pass
+
+def occupato(self):
+    pass
+
+def numero_del_posto(self):
+    pass
+
+def fila(self):
+    pass
+
+
+class PostoVIP(Posto):
+    pass
+
+def prenota(self):
+    pass
+
+
+
+class Teatro(Posto):
+    pass
+
+def aggiungi_posto(self):
+    pass
+
+def prenota_posto(self):
+    pass
+
+def stampa_posti(self):
+    pass
+
