50d30c90595f4e666a781fc8b1e24abc0c804b1a
Solace-hub <fbxeim@gmail.com>
2024-11-06 15:39:01 +0100

    A

--- Corso Python e Machine Learning/5 Novembre/Esercizio Prodotto.py
+++ Corso Python e Machine Learning/5 Novembre/Esercizio Prodotto.py
@@ -1,87 +1,87 @@
 class Prodotto:
-    def __init__(self, nome, costo_produzione, prezzo_vendita):
+    def ____init____(self, nome, costo__produzione, prezzo__vendita):
         self.nome = nome
-        self.costo_produzione = costo_produzione
-        self.prezzo_vendita = prezzo_vendita
+        self.costo__produzione = costo__produzione
+        self.prezzo__vendita = prezzo__vendita
 
-    def calcola_profitto(self):
-        return self.prezzo_vendita - self.costo_produzione
+    def calcola__profitto(self):
+        return self.prezzo__vendita - self.costo__produzione
 
 class Elettronica(Prodotto):
-    def __init__(self, nome, costo_produzione, prezzo_vendita, garanzia):
-        super().__init__(nome, costo_produzione, prezzo_vendita)
-        self.__garanzia = garanzia
+    def ____init____(self, nome, costo__produzione, prezzo__vendita, garanzia):
+        super().____init____(nome, costo__produzione, prezzo__vendita)
+        self.____garanzia = garanzia
 
-    def get_garanzia(self):
-        return self.__garanzia
+    def get__garanzia(self):
+        return self.____garanzia
 
-    def set_garanzia(self, garanzia):
-        self.__garanzia = garanzia
+    def set__garanzia(self, garanzia):
+        self.____garanzia = garanzia
 
 class Abbigliamento(Prodotto):
-    def __init__(self, nome, costo_produzione, prezzo_vendita, materiale):
-        super().__init__(nome, costo_produzione, prezzo_vendita)
-        self.__materiale = materiale
+    def ____init____(self, nome, costo__produzione, prezzo__vendita, materiale):
+        super().____init____(nome, costo__produzione, prezzo__vendita)
+        self.____materiale = materiale
 
-    def get_materiale(self):
-        return self.__materiale
+    def get__materiale(self):
+        return self.____materiale
     
-    def set_materiale(self, materiale):
-        self.__materiale = materiale
+    def set__materiale(self, materiale):
+        self.____materiale = materiale
 
 class Fabbrica:
-    def __init__(self):
+    def ____init____(self):
         self.inventario = {}
 
-    def aggiungi_prodotto(self, prodotto, quantita):
+    def aggiungi__prodotto(self, prodotto, quantita):
         if prodotto in self.inventario:
             self.inventario[prodotto] += quantita
         else:
             self.inventario[prodotto] = quantita
         print(f"Il prodotto '{prodotto.nome}' è stato aggiunto con successo.")
 
-    def vendi_prodotto(self, prodotto, quantita):
+    def vendi__prodotto(self, prodotto, quantita):
         if prodotto in self.inventario and self.inventario[prodotto] >= quantita:
             self.inventario[prodotto] -= quantita
-            profitto = prodotto.calcola_profitto() * quantita
-            self._calcola_totale_profitto(profitto)
+            profitto = prodotto.calcola__profitto() * quantita
+            self.__calcola__totale__profitto(profitto)
             print(f"Il prodotto '{prodotto.nome}' è stato venduto con successo e il profitto è: {profitto}")
         else:
             print("Quantità insufficiente")
 
-    def resi_prodotto(self, prodotto, quantita):
+    def resi__prodotto(self, prodotto, quantita):
         if prodotto in self.inventario:
             self.inventario[prodotto] += quantita
             print(f"{quantita} prodotto '{prodotto.nome}' ricevute come reso.")
 
-    def _calcola_totale_profitto(self, profitto):
-        if hasattr(self, '_totale_profitto'):
-            self._totale_profitto += profitto
+    def __calcola__totale__profitto(self, profitto):
+        if hasattr(self, '__totale__profitto'):
+            self.__totale__profitto += profitto
         else:
-            self._totale_profitto = profitto
+            self.__totale__profitto = profitto
 
-    def get_totale_profitto(self):
-        return getattr(self, '_totale_profitto', 0)
-
-
+    def get__totale__profitto(self):
+        return getattr(self, '__totale__profitto', 0)
 
 
-def descrizione_prodotto(prodotto):
-    descrizione_prodotto = (f"Nome: {prodotto.nome}, Prezzo di vendita: {prodotto.prezzo_vendita}€, Profitto: {prodotto.calcola_profitto()}€")
+
+
+def descrizione__prodotto(prodotto):
+    descrizione__prodotto = (f"Nome: {prodotto.nome}, Prezzo di vendita: {prodotto.prezzo__vendita}€, Profitto: {prodotto.calcola__profitto()}€")
     if isinstance(prodotto, Elettronica):
-        return descrizione_prodotto + f", Garanzia di tot {prodotto.get_garanzia()} anni"
+        return descrizione__prodotto + f", Garanzia di tot {prodotto.get__garanzia()} anni"
     elif isinstance(prodotto, Abbigliamento):
-        return descrizione_prodotto + f", Materiale di questa qualità: {prodotto.get_materiale()}"
+        return descrizione__prodotto + f", Materiale di questa qualità: {prodotto.get__materiale()}"
     
-    return descrizione_prodotto
+    return descrizione__prodotto
 
                 
 #Esempio
 telefono = Elettronica("Smartphone", 150, 350, 1)
 maglietta = Abbigliamento("T-Shirt", 10, 35, "Cotone")
 
-print(descrizione_prodotto(telefono))   
-print(descrizione_prodotto(maglietta)) 
+print(descrizione__prodotto(telefono))   
+print(descrizione__prodotto(maglietta)) 
 
 
     
