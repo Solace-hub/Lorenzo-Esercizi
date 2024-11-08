3deec082f3a1562b0c02501dcb752d517500017a
Solace-hub <fbxeim@gmail.com>
2024-11-08 11:19:55 +0100

    Teoria, esempi, esercizio 1

--- Corso Python e Machine Learning/8 Novembre/Esercizio 1 Numpy.py
+++ Corso Python e Machine Learning/8 Novembre/Esercizio 1 Numpy.py
@@ -1,4 +1,4 @@
-import numpy as np
+'''import numpy as np
 
 array = np.arange(10, 49)
 print(array.dtype)
@@ -6,4 +6,41 @@ print(array.shape)
 
 array.astype(np.float64)
 print(array.dtype)
-print(array.shape)
+print(array.shape)'''
+
+
+import numpy as np
+
+'''class OrganizzatoreArray:
+    def __init__(self, start, end):
+        self.array = np.arange(start, end)
+    
+    def array_tipo(self):
+        return self.array.dtype
+    
+    def array_reshaped(self):
+        return self.array.shape
+    
+    def converti_array(self):
+        self.array = self.array.astype(np.float64)
+    
+    def mostra_info(self):
+        print("Tipo di dato:", self.array_tipo())
+        print("Forma dell'array:", self.array_reshaped())
+
+
+
+array1 = OrganizzatoreArray(10, 50)
+array1.mostra_info()
+
+array1.converti_array()
+array1.mostra_info()'''
+
+
+
+
+
+
+    
+
+
--- Corso Python e Machine Learning/8 Novembre/Numpy.py
+++ Corso Python e Machine Learning/8 Novembre/Numpy.py
@@ -19,3 +19,31 @@ arr = np.arange(6)
 reshaped_arr = arr.reshape((6,1))
 print(reshaped_arr)
 
+#//
+
+
+arr_2d = np.array([[1,2,3,4], [5,6,7,8], [9,10,11,12]])
+
+print(arr_2d[1:3])
+print(arr_2d[:, 1:3])
+print(arr_2d[1:, 1:3])
+
+
+#array[start:stop:step] Slicing  Start inclusivo, stop esclusivo, step tra un indice e l'altro
+
+print(arr[2:7]) #base
+print(arr[1:8:2]) #con passo
+print(arr[:5]) #start
+print(arr[5:]) #stop
+print(arr[-5:]) #negativi
+
+#Fancy indexing
+arr = np.array([10, 20, 30, 40, 50])
+
+indices = np.array([1, 3])
+print(arr[indices])
+
+indices = [0,2,4]
+print(arr[indices])
+
+
